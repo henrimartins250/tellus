6028ea63b9e8b41bc7f350e2a2714464295caf22
Henrimartins250 <henrinkcstd@gmail.com>
2026-06-10 20:11:43 -0300

    chore: set up flake

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "tellus devshell";
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+  }:
+    utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            # firmware dependencies
+
+            # db dependencies
+
+            # rust dependencies
+
+            # frontend dependencies
+          ];
+        };
+      }
+    );
+}
